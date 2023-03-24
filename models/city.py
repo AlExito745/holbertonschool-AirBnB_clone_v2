ede3fe1cb3bb89c9e32ea4804104f854d6c97592
AlExito745 <alxtinoco1988@gmail.com>
2023-03-24 14:52:14 -0700

    fix: correction inherence

--- models/city.py
+++ models/city.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+import sqlalchemy
 from sqlalchemy import Column, Integer, String, ForeignKey
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
