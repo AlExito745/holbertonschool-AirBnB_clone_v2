6743a6b636875e5cb75d7883d59a058dd09ff5d3
KevinYeff <kevinyeff94@gmail.com>
2023-03-25 22:48:48 -0500

    feat: defining amenity class

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" Amenity Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship, backref
 
 
 class Amenity(BaseModel):
-    name = ""
+    """
+    Inherits from BaseModel and Base, amenities for the database
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship(
+        "Place", secondary="place_amenity", backref="amenities")
