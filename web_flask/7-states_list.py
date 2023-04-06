1b80aa19805be1eecb138273a4f191328a3fa8fb
AlExito745 <alxtinoco1988@gmail.com>
2023-04-06 16:07:38 -0700

    feat: script that start a flask web application

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+
+from flask import Flask
+from flask import render_template
+from models import storage
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def handle_teardown(self):
+    """method to handle teardown"""
+    storage.close()
+
+
+@app.route("/states_list")
+def state_list():
+    """method to render states"""
+    states = storage.all("State").values()
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ == "__main__":
+        app.run(host='0.0.0.0', port=5000)
